a58cca1705d43a038eb3e41eea7df82da77f3e35
Unknown <jean.bellio@acad.pucrs.br>
2018-11-25 22:03:59 -0200

    Remoção do User

--- src/main/java/com/fitnesshouse/api/documents/Student.java
+++ src/main/java/com/fitnesshouse/api/documents/Student.java
@@ -2,11 +2,19 @@ package com.fitnesshouse.api.documents;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-public class Student extends User {
+public class Student {
 
+	@Id
+	private String id;
+	private String name;
+	private String email;
+	private long age;
+	private String sex;
+	private String password;
 	private List<WorkoutSheet> workoutSheet;
 	private String idTeacher;
 	private List<Message> messages;
@@ -16,13 +24,67 @@ public class Student extends User {
 		super();
 	}
 
-	public Student(List<WorkoutSheet> workoutSheet, String idTeacher, List<Message> messages,
-			List<Feedback> feedbacks) {
+	public Student(String id, String name, String email, long age, String sex, String password,
+			List<WorkoutSheet> workoutSheet, String idTeacher, List<Message> messages, List<Feedback> feedbacks) {
 		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.age = age;
+		this.sex = sex;
+		this.password = password;
 		this.workoutSheet = workoutSheet;
 		this.idTeacher = idTeacher;
 		this.messages = messages;
 		this.feedbacks = feedbacks;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getAge() {
+		return age;
+	}
+
+	public void setAge(long age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public List<WorkoutSheet> getWorkoutSheet() {
@@ -57,5 +119,4 @@ public class Student extends User {
 		this.feedbacks = feedbacks;
 	}
 
-	
 }
